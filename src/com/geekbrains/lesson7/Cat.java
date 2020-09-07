97946247d350e403b0359d51705f6e022717ace0
glier <borozdenets@gmail.com>
2020-09-07 23:17:35 +0300

    Lesson 7

---
+++ src/com/geekbrains/lesson7/Cat.java
@@ -0,0 +1,23 @@
+package com.geekbrains.lesson7;
+
+public class Cat {
+    private final String name;
+    private final int appetite;
+    private boolean wellFed = false;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+    public void eat(Plate p) {
+        wellFed = p.decreaseFood(appetite);
+    }
+
+    public boolean isWellFed() {
+        return wellFed;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/com/geekbrains/lesson7/Main.java
@@ -0,0 +1,28 @@
+package com.geekbrains.lesson7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static final int CATS_COUNT = 10;
+    private static final int FOOD_COUNT = 100;
+    private static final int[] APPETITE_INTERVAL = {1, 50};
+
+    public static void main(String[] args) {
+        Plate plate = new Plate(FOOD_COUNT);
+        List<Cat> cats = new ArrayList<>();
+
+        for (int i = 0; i < CATS_COUNT; i++) {
+            cats.add(new Cat("Кот " + i, getAppetite()));
+            cats.get(i).eat(plate);
+            System.out.println(cats.get(i).getName() + " сыт? - " + cats.get(i).isWellFed());
+            plate.info();
+        }
+
+    }
+
+    public static int getAppetite() {
+        return APPETITE_INTERVAL[0] + (int) (Math.random() * APPETITE_INTERVAL[1]);
+    }
+}
